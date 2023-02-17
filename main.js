eae2020f7813c280a09e35ab088c79d60fa6c499
valerie2107 <kinkamyl@hotmail.com>
2023-02-17 13:28:05 +0100

    icones

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 function toggleFun(){
-    isFun = localStorage.getItem("ls_funfun");
+    isFun = localStorage.getItem("locStor_funfun");
     if(isFun==='true'){
-        localStorage.setItem('ls_funfun','false');
+        localStorage.setItem('locStor_funfun','false');
         body.classList.remove("funfun");
         console.log("classical mode chargé");
     }else{
-        localStorage.setItem('ls_funfun', 'true');
+        localStorage.setItem('locStor_funfun', 'true');
         body.classList.add("funfun");
         console.log('funmode chargé');
     }
 }
 
-function toggleMode(){  //toggleMode car réagit a chaque clic.
-    isDark = localStorage.getItem("ls_dark");
+function toggleDark(){  //toggleDark car réagit a chaque clic.
+    isDark = localStorage.getItem("locStor_dark");
     if(isDark==='true'){    //les booléens sont automatiquement changés en string par localstorage (JSON??)
-        localStorage.setItem("ls_dark",'false');
+        localStorage.setItem("locStor_dark",'false');
         body.classList.remove("dark");
         console.log("darkmode chargé");
     }else{
-        localStorage.setItem("ls_dark",'true');
+        localStorage.setItem("locStor_dark",'true');
         body.classList.add("dark");
         console.log("lightmode chargé");
     }
 }
 
 let body = document.querySelector("body");
-let isDark = localStorage.getItem("ls_dark"); /*est-ce que le user a déjà mis qq chose dans le local storage? vérifions*/
-let isFun = localStorage.getItem("ls_funfun");/*est-ce que le user a déjà mis qq chose dans le local storage? vérifions*/
+let isDark = localStorage.getItem("locStor_dark"); /*est-ce que le user a déjà mis qq chose dans le local storage? vérifions*/
+let isFun = localStorage.getItem("locStor_funfun");/*est-ce que le user a déjà mis qq chose dans le local storage? vérifions*/
 let darkModeButton = document.querySelector("#dark_mode");
 let funModeButton = document.querySelector("#funfun");
 
-darkModeButton.addEventListener("click",toggleMode);
+darkModeButton.addEventListener("click",toggleDark);
 funModeButton.addEventListener("click", toggleFun);
 
 if(isDark==='true') { //les booléens sont automatiquement changés en string par localstorage (JSON??)
